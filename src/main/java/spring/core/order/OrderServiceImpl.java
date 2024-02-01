aac1c0e9fd57313d96eaea2d9ad361ee82bc58b1
nabu0305 <sohwi0305@gmail.com>
2024-02-01 21:02:42 +0900

    주문과 할인 도메인 실행과 테스트

--- src/main/java/spring/core/order/OrderServiceImpl.java
+++ src/main/java/spring/core/order/OrderServiceImpl.java
@@ -1,2 +1,23 @@
-package spring.core.order;public class OrderServiceImpl {
+package spring.core.order;
+
+import spring.core.discount.DiscountPoilcy;
+import spring.core.discount.FixDiscountPolicy;
+import spring.core.member.Member;
+import spring.core.member.MemberRepository;
+import spring.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPoilcy discountPoilcy = new FixDiscountPolicy();
+
+
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPoilcy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName,itemPrice,discountPrice);
+    }
 }

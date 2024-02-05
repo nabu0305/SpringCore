6d55e4e37a041125af70e2244bb16755d35b53b1
nabu0305 <sohwi0305@gmail.com>
2024-02-05 20:07:44 +0900

    Appconfig 리팩터링

--- src/main/java/spring/core/Appconfig.java
+++ src/main/java/spring/core/Appconfig.java
@@ -1,6 +1,8 @@
 package spring.core;
 
+import spring.core.discount.DiscountPoilcy;
 import spring.core.discount.FixDiscountPolicy;
+import spring.core.member.MemberRepository;
 import spring.core.member.MemberService;
 import spring.core.member.MemberServiceImpl;
 import spring.core.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import spring.core.order.OrderServiceImpl;
 public class Appconfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());//생성자 주입
+        return new MemberServiceImpl(memberRepository());//생성자 주입
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPoilcy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }

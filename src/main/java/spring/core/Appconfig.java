c24bed7d3d1ba1ebba9220d88e5e5359e88f9c77
nabu0305 <sohwi0305@gmail.com>
2024-02-09 18:44:49 +0900

    @Comfigutation과 싱글톤

--- src/main/java/spring/core/Appconfig.java
+++ src/main/java/spring/core/Appconfig.java
@@ -17,16 +17,19 @@ public class Appconfig {
 
     @Bean
     public MemberService memberService(){
+        System.out.println("call Appconfig.memberService");
         return new MemberServiceImpl(memberRepository());//생성자 주입
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call Appconfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call Appconfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
---
+++ src/test/java/spring/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,34 @@
+package spring.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.core.Appconfig;
+import spring.core.member.MemberRepository;
+import spring.core.member.MemberServiceImpl;
+import spring.core.order.OrderServiceImpl;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(Appconfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberService -> memberRepository = " + memberRepository1);
+        System.out.println("orderService -> memberRepository = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+
+
+    }
+}

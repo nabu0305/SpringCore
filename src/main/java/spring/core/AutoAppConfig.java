846d4358c3d7e57aa1ed3d025cdd9b82d10451fc
nabu0305 <sohwi0305@gmail.com>
2024-02-12 00:40:24 +0900

    [200~basePackages : 탐색할 패키지의 시작 위치를 지정한다. 이 패키지를 포함해서 하위 패키지를 모두 탐색한다.

--- src/main/java/spring/core/AutoAppConfig.java
+++ src/main/java/spring/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "spring.core.member",
         excludeFilters =  @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 ) //@붙은 에노테이션을 다 찾아서 스프링 빈으로 자동등록
 public class AutoAppConfig {

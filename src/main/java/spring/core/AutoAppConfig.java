14c6fe2b506fb198673bea6de05c28aecf59c7a4
nabu0305 <sohwi0305@gmail.com>
2024-02-15 21:17:29 +0900

    이 경우 수동 빈 등록이 우선권을 가진다. (수동 빈이 자동 빈을 오버라이딩 해버린다.)

--- src/main/java/spring/core/AutoAppConfig.java
+++ src/main/java/spring/core/AutoAppConfig.java
@@ -1,8 +1,11 @@
 package spring.core;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import spring.core.member.MemberRepository;
+import spring.core.member.MemoryMemberRepository;
 
 @Configuration
 @ComponentScan(
@@ -10,5 +13,8 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters =  @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 ) //@붙은 에노테이션을 다 찾아서 스프링 빈으로 자동등록
 public class AutoAppConfig {
-    
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
